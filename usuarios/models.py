b45b94335c01c49e98470fcbcd5d60486b086b94
0721santi <san200526@gmail.com>
2023-04-04 02:21:05 -0500

    Conexión base de datos mysql

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class usuario(models.Model):
+    name = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=40)
+    location = models.CharField(max_length=50)
+    age = models.PositiveIntegerField(default=0)
